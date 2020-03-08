3836bf16ddfaaf02975670bc0c970c63fa6e9a4d
laarchambault <laarchambault@gmail.com>
2020-03-08 17:40:13 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Laura, world's best mom."
+    end
+
+    get '/hometown' do
+        "My hometown is Byron, quaint and friendly."
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'King of Glory' by Shana Wilson Williams."
+    end
 end
